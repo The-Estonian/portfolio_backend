686b7057dfd3952d8d7bec45c67a1bae1ee68659
jaanus <zaar2213@gmail.com>
2025-02-17 00:20:40 +0200

    added options preflight for cors

--- components/middleware/corsMiddleware.js
+++ components/middleware/corsMiddleware.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 const corsOptions = {
   origin: 'https://the-estonian.com',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
