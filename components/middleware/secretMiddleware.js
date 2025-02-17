7d7e0d862fbfdba8fe8940d6969f09e04ab8da55
jaanus <zaar2213@gmail.com>
2025-02-17 18:02:58 +0200

    added new secret for client

--- components/middleware/secretMiddleware.js
+++ components/middleware/secretMiddleware.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const secretMiddleware = (req, res, next) => {
   console.log('Secret check!');
 
   const secretToken = req.get('X-Client-Secret');
 
-  if (secretToken === 'estonian') {
+  // OUTSOURCE THI
+  if (secretToken === process.env.CLIENT_SECRET) {
     next();
   } else {
     return res.status(403).send('Forbidden: Unknown client');
